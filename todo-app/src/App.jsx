12dcb408b8bc841c1ade9d97b5a8441e11da09aa
ilovepotato05 <lmyee1220@gmail.com>
2026-05-10 01:04:45 +0800

    initial commit

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Cloud To-Do App</h1>
+      <p>My First deployment on Render</p>
+    </div>
+  );
+}
+
+export default App;

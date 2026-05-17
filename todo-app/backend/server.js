71a951c3897a8627c0add7af6e7cb98ff0f054ef
ilovepotato05 <lmyee1220@gmail.com>
2026-05-17 22:43:43 +0800

    change port number

--- todo-app/backend/server.js
+++ todo-app/backend/server.js
@@ -79,7 +79,7 @@ app.delete("/api/todos/:id", async (req, res) => {
     res.json(mapTask(removed));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
